3a444288ba16350d56860dc1dd4f3a248f131448
Himanshu <himanshum50360@gmail.com>
2022-03-21 17:05:11 +0530

    first commit

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ io.on("connection", (socket) => {
 //     });
 // });
 
-server.listen(3000, () => {
+server.listen(process.env.port || 3000, () => {
     console.log("Server is running on port 3000.");
 });
